71dba42c22035715fc5e0ff10ba095a15ba08ce9
Justin Gottshall <justin.gottshall@sherwin.com>
2019-11-03 15:48:28 -0500

    add a landing page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: ``,
+    description: ``,
+    author: ``,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -14,7 +14,20 @@ module.exports = {
       },
     },
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    {
+		  resolve: `gatsby-plugin-sharp`,
+			options: {
+		    useMozJpeg: true,
+				stripMetadata: true,
+				defaultQuality: 90,
+			},
+		},
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/images/`
+      }
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
